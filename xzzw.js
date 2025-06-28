4e97a364b9acb4d83bdb49be6b3bd1b3589b87da
shini74744 <158538512+shini74744@users.noreply.github.com>
2025-06-28 12:39:42 +0800

    Update xzzw.js

--- xzzw.js
+++ xzzw.js
@@ -15,7 +15,7 @@
             z: 10, // z-index 可自定义
             o: getAttr(currentScript, "opacity", 0.5),
             c: getAttr(currentScript, "color", "0,0,0"),
-            n: parseInt(getAttr(currentScript, "count", 60))  // 点数量默认60
+            n: parseInt(getAttr(currentScript, "count", 50))  // 点数量默认60
         };
     }
 
